1faedbe35d794d9a6da028fbb78d2d79c23be0ca
Did'Yeah <127863515+didYeah@users.noreply.github.com>
2023-06-02 16:05:32 +0200

    202306021605

--- src/app/truncate.pipe.ts
+++ src/app/truncate.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class TruncatePipe implements PipeTransform {
-  transform(value: string, args: unknown[]): string {
+  transform(value: string): string {
     const maxLength: number = 10;
     if (value.length > maxLength) {
       return value.substring(0, maxLength) + '...';
