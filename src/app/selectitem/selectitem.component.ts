63808732d51a8840d9f5ad60fffcf9f1bcd8cdeb
DidYeah <didierphilibert@gmail.com>
2023-06-02 09:45:52 +0200

    Update 20230602 0945

--- src/app/selectitem/selectitem.component.ts
+++ src/app/selectitem/selectitem.component.ts
@@ -9,13 +9,23 @@ export class SelectitemComponent implements OnInit {
   @Input() allItems: [string, boolean][] =[]; // Variable d'entrée reprenant tous les items de la liste
   @Output() updateItems: EventEmitter<[string, boolean][]> = new EventEmitter<[string, boolean][]>(); // Variable de sortie permettant de stocker les items sélectionnés et de les mettre à jour
 
-  selectAll = (): void =>{ // Sélection de tous les items en leur définissant le boolean à true
+  /*selectAll = (): void =>{ // Sélection de tous les items en leur définissant le boolean à true
     this.allItems = this.allItems.map(([item, _]) => [item, true]);
     this.updateItems.emit(this.allItems);
   }
 
   unselectAll = (): void =>{ // Sélection de tous les items en leur définissant le boolean à false
     this.allItems = this.allItems.map(([item]) => [item, false]);
+    this.updateItems.emit(this.allItems);
+  }*/
+
+  selectAll() {
+    this.allItems.forEach(item => (item[1] = true));
+    this.updateItems.emit(this.allItems);
+  }
+
+  unselectAll() {
+    this.allItems.forEach(item => (item[1] = false));
     this.updateItems.emit(this.allItems);
   }
 
